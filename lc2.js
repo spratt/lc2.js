8706937b26c1d56076f4003a87bf255e45b08330
Simon Pratt <simon@pr4tt.com>
2013-04-19 01:21:29 -0400

    Added logging, implemented 2 types of adding

--- lc2.js
+++ lc2.js
@@ -8,12 +8,16 @@
 // and Sanjay J. Patel
 
 var LC2 = (function(LC2) {
+	// constants
 	var BASE = 2;
 	var BITS = 16;
 	var REGISTERS = 16;
-	
-	var ProtoLC2 = {};
 
+	var COND_NEG  = 1 << 0; // 2^0 = 1
+	var COND_POS  = 1 << 1; // 2^1 = 2
+	var COND_ZERO = 1 << 2; // 2^2 = 4
+
+	// register class
 	var Register = function(_val) {
 		if(!_val) var _val = 0;
 
@@ -24,8 +28,66 @@ var LC2 = (function(LC2) {
 		this.__defineSetter__("value", function(val){
 			_val = val % Math.pow(BASE,BITS);
 		});
+
+		this.toString = function() {
+			return "reg[" + _val + "]";
+		};
 	};
 
+	// helpers
+	var log = function(o) {
+		console.log(o);
+	};
+	
+	var ones = function(n) {
+		var value = 0;
+		for(var i = 0; i < n; ++i) {
+			value = value | (1 << i);
+		}
+		return value;
+	}
+	
+	var set_conditions = function(lc2_inst, value) {
+		if(value < 0)
+			lc2_inst.conds = COND_NEG;
+		else if(value > 0)
+			lc2_inst.conds = COND_POS;
+		else
+			lc2_inst.conds = COND_ZERO;
+		log("conds set to " + lc2_inst.conds);
+	};
+
+	var add_reg_to_reg = function(lc2_inst,dest_reg,src_reg1,src_reg2) {
+		var val1 = lc2_inst.reg[src_reg1].value;
+		var val2 = lc2_inst.reg[src_reg2].value;
+		var result = val1 + val2;
+		log(val1 + " + " + val2 + " = " + result);
+		lc2_inst.reg[dest_reg].value = result;
+		set_conditions(lc2_inst,result);
+	};
+
+	var add_reg_to_imm5 = function(lc2_inst,dest_reg,src_reg1,imm5) {
+		var val1 = lc2_inst.reg[src_reg1].value;
+		var val2 = imm5 & ones(5);
+		var result = val1 + val2;
+		log(val1 + " + " + val2 + " = " + result);
+		lc2_inst.reg[dest_reg].value = result;
+		set_conditions(lc2_inst,result);
+	};
+
+	// methods
+	var ProtoLC2 = {};
+	
+	ProtoLC2.add = function(dest_reg,src_reg1,imm5_bit,last) {
+		log("add(" + dest_reg + "," + src_reg1 + "," +
+			imm5_bit + "," + last + ")");
+		if(imm5_bit === 0)
+			add_reg_to_reg(this,dest_reg,src_reg1,last);
+		else if(imm5_bit === 1)
+			add_reg_to_imm5(this,dest_reg,src_reg1,last);
+	};
+
+	// initialization
 	var initialize_registers = function(lc2_inst) {
 		for(var i = 0; i < REGISTERS; ++i) {
 			lc2_inst.reg[i] = new Register(0);
@@ -38,8 +100,8 @@ var LC2 = (function(LC2) {
 		}
 	};
 
-	// initialization
 	var LC2 = function() {
+		this.conds = 0;
 		this.mem = [];
 		this.reg = [];
 		this.pc = new Register(0);
