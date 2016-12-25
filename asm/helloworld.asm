1e7426a782ee2599abfe0297e6b312923def295d
Simon Pratt <simon@pr4tt.com>
2016-12-25 06:36:45 -0500

    Intro to assembly editor

--- asm/helloworld.asm
+++ asm/helloworld.asm
@@ -1,5 +1,13 @@
-;;; helloworld.asm
-;;; Purpose: prints "Hello, world!" and halts
+;;; Welcome to the Assembly Editor.  This is the editor, in which you can write
+;;; LC-2 assembly code and it (should be) nicely highlighted.
+
+;;; If you click the button below that is labeled "Assemble", this code will be
+;;; assembled into bytecode and loaded into the LC-2 simulator whose state is
+;;; shown below.
+
+;;; On the right side of the LC-2 state, there is a button labeled "Step."
+;;; Clicking on this button executes the code at the address stored in the PC
+;;; register.
 
         ;; set the start address to 3000 (in hex)
         .orig   $3000
